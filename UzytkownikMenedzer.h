490d4521ad628b433a05c5251007d6914670504d
Patryk Sobilo <patryk.sobilo.programista@gmail.com>
2024-03-06 19:03:37 +0100

    Podzial KsiazkaAdresowa na mniejsze klasy

---
+++ UzytkownikMenedzer.h
@@ -0,0 +1,32 @@
+#ifndef UZYTKOWNIKMENEDZER_H
+#define UZYTKOWNIKMENEDZER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "PlikZUzytkownikami.h"
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class UzytkownikMenedzer
+{
+    int idZalogowanegoUzytkownika;
+    vector <Uzytkownik> uzytkownicy;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+    PlikZUzytkownikami plikZUzytkownikami;
+
+
+public:
+    void wypiszWszystkichUzytkownikow();
+    void rejestracjaUzytkownika();
+
+};
+
+#endif
